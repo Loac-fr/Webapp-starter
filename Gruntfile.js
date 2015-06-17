7993e6cb5fd6c642508d01671fac0c4ea79d5f61
Loic <Loac-fr@users.noreply.github.com>
2015-06-17 16:44:16 +0200

    load tasks using grunt-load-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,10 +24,15 @@
 
 module.exports = function(grunt) {
 
+  // Load grunt modules =======================/
+
+  require('load-grunt-tasks')(grunt);
+
   // CONFIG ===================================/
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
 
     // clean dist folder
 
@@ -49,7 +54,7 @@ module.exports = function(grunt) {
       img : {
         expand : true,
         cwd : 'app/img/',
-        src : [ '*' ],
+        src : [ '**/**' ],
         dest : 'dist/img',
       },
       fonts : {
@@ -172,7 +177,7 @@ module.exports = function(grunt) {
     watch: {
       scripts: {
         files: ['app/js/**/*.js'],
-        tasks: [ 'concat', 'notify:concat' ], // ['concat', 'uglify']
+        tasks: [ 'concat', 'notify:concat' ],
         options: {
             spawn: false,
             livereload: true
@@ -225,18 +230,6 @@ module.exports = function(grunt) {
 
   });
 
-  // DEPENDENT PLUGINS =========================/
-
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-php');
-  grunt.loadNpmTasks('grunt-notify');
-  grunt.loadNpmTasks('grunt-autoprefixer');
 
   // TASKS =====================================/
 
@@ -251,10 +244,12 @@ module.exports = function(grunt) {
     Notes:
 
     When registering a new Task we can also pass in any other registered Tasks.
-    e.g. grunt.registerTask('release', 'default requirejs'); // when running this task we also run the 'default' Task
+    e.g. grunt.registerTask('release', 'default requirejs'); 
+    // when running this task we also run the 'default' Task
 
     We don't do this above as we would end up running `sass:dev` when we only want to run `sass:dist`
-    We could do it and `sass:dist` would run afterwards, but that means we're compiling sass twice which (although in our example quick) is extra compiling time.
+    We could do it and `sass:dist` would run afterwards, but that means we're compiling 
+    sass twice which (although in our example quick) is extra compiling time.
 
     To run specific sub tasks then use a colon, like so...
     grunt sass:dev
